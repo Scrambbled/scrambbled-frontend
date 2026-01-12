0600303e393bb03bd287a516fab351b5f0d9fe1d
daniel.wiszniowski <mrcabbagestick@gmail.com>
2026-01-12 13:35:34 +0100

    Styling home view

--- src/views/HomeView.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1>HEJKA TU LENKA</h1>
-</template>
---
+++ src/views/HomeView/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+import "./HomeView.scss"
+
+</script>
+
+<template>
+    <div class="home-view">
+        <div class="session">
+            <button class="session-button">Join Game</button>
+            <button class="session-button">Create Session</button>
+        </div>
+
+
+        <div class="profile">
+            <div class="profile-icon">
+                <img src="" alt="">
+            </div>
+
+            <input type="text" class="username">
+        </div>
+    </div>
+</template>
