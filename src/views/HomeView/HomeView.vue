44c49c3666cfa05670d5c3d9ae505d20d1d9f37e
daniel.wiszniowski <mrcabbagestick@gmail.com>
2026-01-21 12:50:20 +0100

    Add sliding menu

--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
-import "./HomeView.scss"
+import { ref } from "vue";
 import ProfileEdit from "../../components/ProfileEdit.vue";
+
+const currentMenuIndex = ref(1)
 
 </script>
 
 <template>
     <div class="home-view">
         <div class="session framed-box">
-            <button class="session-button press-in-button">Join Game</button>
-            <button class="session-button press-in-button">Create Session</button>
+            <div class="moving-frame" :style="{'--frame': currentMenuIndex}">
+                <div class="create-session-menu">
+                    No thoughts… Menu empty :drooling:
+                </div>
+                <div class="main-menu">
+                    <button class="session-button press-in-button" @click.prevent="currentMenuIndex = 2">Join Game</button>
+                    <button class="session-button press-in-button" @click.prevent="currentMenuIndex = 0">Create Session</button>
+                </div>
+                <div class="join-game-menu">
+                    <input type="text" class="session-code text-input">
+                    <button class="session-button press-in-button">Join Game</button>
+                </div>
+            </div>
         </div>
 
        <ProfileEdit class="framed-box"/>
@@ -33,10 +46,34 @@ import ProfileEdit from "../../components/ProfileEdit.vue";
     height: 500px;
 }
 
-.session{
+.main-menu, .join-game-menu{
     display: grid;
     grid-template-rows: repeat(2, 4rem);
     gap: 1rem;
     align-content: center;
+}
+
+.session{
+    position: relative;
+    overflow-x: hidden;
+}
+
+.moving-frame{
+    --frame: 0;
+
+    height: 100%;
+
+    display: grid;
+    grid-auto-flow: column;
+    grid-auto-columns: 100%;
+
+    transform: translateX(calc(var(--frame) * -100%));
+
+    transition: transform 250ms ease-in-out;
+
+    & > *{
+        padding: 1rem;
+        height: 100%;
+    }
 }
 </style>
