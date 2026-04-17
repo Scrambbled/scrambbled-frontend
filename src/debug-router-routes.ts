c932d1e9ec8293ea70efad7868ca5b0cb2f8498b
daniel.wiszniowski <mrcabbagestick@gmail.com>
2026-04-17 22:35:34 +0200

    Add debug routes

---
+++ src/debug-router-routes.ts
@@ -0,0 +1,5 @@
+import WordInSentence from "./games/word_in_sentence/WordInSentence.vue";
+
+export const debugRoutes = [
+    {path: "/words_in_words", component: WordInSentence}
+]
--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,13 @@
-import {createMemoryHistory, createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 
 import HomeView from './views/HomeView/HomeView.vue'
 import WordInSentence from './games/word_in_sentence/WordInSentence.vue'
 import Game from './games/Game.vue'
+import { debugRoutes } from './debug-router-routes'
 
 const routes = [
     {path: "/game", component: Game},
-    { path: "/", component: HomeView},
+    {path: "/", component: HomeView},
     {path: "/word", component: WordInSentence},
 ]
 
@@ -14,3 +15,10 @@ export const router = createRouter({
     history: createWebHistory(),
     routes
 })
+
+// Add debug routes to view components
+if(import.meta.env.VITE_DEBUG){
+    for(const route of debugRoutes){
+        router.addRoute({path: `/debug${route.path}`, component: route.component})
+    }
+}
