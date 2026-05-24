8662d9e87708acdabef9986021f3d230c1956aec
daniel.wiszniowski <mrcabbagestick@gmail.com>
2026-05-25 00:41:56 +0200

    Add "result" to name generator

--- src/misc/usernames.ts
+++ src/misc/usernames.ts
@@ -1,6 +1,6 @@
 import { randomPick } from "./tools"
 
-const FIRST_NAME_PARTS = ["Wild", "Fantastic", "Odd", "Weird", "Electric", "Furious"]
+const FIRST_NAME_PARTS = ["Wild", "Fantastic", "Odd", "Weird", "Electric", "Furious", "Resolute"]
 const SECOND_NAME_PARTS = ["Racoon", "Fox", "Astronaut", "Firefighter", "SockPuppet"]
 
 export const getRandomName = () => `${randomPick(FIRST_NAME_PARTS)}${randomPick(SECOND_NAME_PARTS)}`
